9d4cf5f035fbf95d5d7c7bfb48aa74aee33e6799
EYRAUD Come <ce.eyraud@gmail.com>
2025-06-14 16:51:50 +0200

    Ajout heatmap page 3

--- layouts/page3.py
+++ layouts/page3.py
@@ -1,10 +1,12 @@
-from dash import html, dcc, Input, Output
+from dash import html, dcc, Input, Output, State
 from components.weekly_heatmap_network import generate_weekly_network_heatmap
 from components.animated_bixi_heatmap import generate_animated_bixi_heatmap
+import geopandas as gpd
 
-def layout(data):
+def layout(page3_viz2_gdf):
     return html.Div([
         html.H2("Fréquentation du Réseau Cyclable & BIXI", className="section-title"),
+        dcc.Store(id='viz2-data', data=page3_viz2_gdf.__geo_interface__),
 
         dcc.Dropdown(
             id='page3-viz-selector',
@@ -22,9 +24,11 @@ def layout(data):
 def register_callbacks(app):
     @app.callback(
         Output('page3-viz-display', 'figure'),
-        Input('page3-viz-selector', 'value')
+        Input('page3-viz-selector', 'value'),
+        State('viz2-data','data')
     )
-    def update_page3_viz(selected_viz):
+    def update_page3_viz(selected_viz,viz2_data):
+        gdf = gpd.GeoDataFrame.from_features(viz2_data["features"])
         if selected_viz == 'bixi':
-            return generate_animated_bixi_heatmap()
+            return generate_animated_bixi_heatmap(gdf)
         return generate_weekly_network_heatmap()
